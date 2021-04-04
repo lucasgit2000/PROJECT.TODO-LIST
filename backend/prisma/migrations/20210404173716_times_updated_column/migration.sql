abfb08f73738c0e86ded5b2026d713c5aadce37e
Lucas Lucena <ml.lucaslucena073@outlook.com>
2021-04-04 14:37:54 -0300

    times updated column and task for idle user route added

---
+++ backend/prisma/migrations/20210404173716_times_updated_column/migration.sql
@@ -0,0 +1,2 @@
+-- AlterTable
+ALTER TABLE "Task" ADD COLUMN     "timesUpdated" INTEGER NOT NULL DEFAULT 0;
--- backend/src/index.js
+++ backend/src/index.js
@@ -38,6 +38,61 @@ app.get("/tasks/:taskStatus", (req, res) => {
 });
 
 /* 
+    Create task 
+*/
+app.post("/tasks", (req, res) => {
+  const { description } = req.body;
+
+  prisma.task
+    .create({
+      data: {
+        description,
+      },
+    })
+    .then((data) => {
+      return res.json([data]);
+    })
+    .catch((err) => {
+      return res.status(400).send({
+        message: err["message"] ? err["message"] : "an error ocurred",
+      });
+    })
+    .finally(async () => {
+      await prisma.$disconnect();
+    });
+});
+
+/* 
+    Create tasks for idle user
+*/
+app.post("/tasks/many/:quantity", (req, res) => {
+    const { quantity } = req.params;
+
+    const tasksToBeCreated = new Array(quantity);
+
+    for (let index = 0; index < tasksToBeCreated.length; index++) {
+        const randomDogFacts = ""; //TODO: ALEX WOHL BRUCK API
+        tasksToBeCreated[index] = {description: randomDogFacts};  
+    }
+  
+    prisma.task
+      .createMany({
+        data: tasksToBeCreated
+      })
+      .then((data) => {
+        return res.json([data]);
+      })
+      .catch((err) => {
+        return res.status(400).send({
+          message: err["message"] ? err["message"] : "an error ocurred",
+        });
+      })
+      .finally(async () => {
+        await prisma.$disconnect();
+      });
+  });
+
+/* 
     Update task status
 */
 app.put("/tasks", (req, res) => {
